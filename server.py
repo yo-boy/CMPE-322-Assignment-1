41b22f067e8ab56fec0d1d794bc9d10c758fb13c
yo_boi <abdelrahmanbbb@gmail.com>
2023-04-22 15:27:32 +0300

    addedd a few comments
    Signed-off-by: yo_boi <abdelrahmanbbb@gmail.com>

--- server.py
+++ server.py
@@ -2,18 +2,21 @@ import socket
 import threading
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 5555  # Port to listen on (non-privileged ports are > 1023)
-ADDRESS = (HOST, PORT)
-USERPASS = "CMPE322\0bilgiuni"
+PORT = 5555  # Port to listen on (non-privileged ports are > 1023), 5555 instead of 555 is just to allow non-privileged execution
+ADDRESS = (HOST, PORT) # handling the address this way is easier
+USERPASS = "CMPE322\0bilgiuni" # we will send user authentication as a null concatenated string
 
 def authenticateClient(client_socket):
+    # get data from the client
     data = client_socket.recv(1024)
+    # return true if the client successfully authenticated
     return data.decode() == USERPASS
 
 def handleClient(client_socket):
     # Send a welcome message to the client
     response = "Hello, client!\nPlease send you login information."
     client_socket.sendall(response.encode())
+    # continually ask the client to authenticate until it authenticates successfully
     while (!authenticateClient()):
         response = "incorrect login information, failed to authenticate, try again."
         client_socket.sendall(response.encode())
